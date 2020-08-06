0a7c37fcfbb89d1208f1776f801e36101d3c706a
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-06 21:08:46 +0100

    added links to reaction role message

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -11,32 +11,32 @@ module.exports = {
             "fields": [
               {
                 "name": ":green_circle:",
-                "value": "Little Improvements: Variated",
+                "value": "[Little Improvements: Variated](https://www.planetminecraft.com/texture-pack/little-improvements-variated/)",
                 "inline": true
               },
               {
                 "name": ":red_circle:",
-                "value": "Better Redstone Components",
+                "value": "[Better Redstone Components](https://www.planetminecraft.com/texture-pack/better-redstone-components/)",
                 "inline": true
               },
               {
                 "name": ":blue_circle:",
-                "value": "Connected Texture Packs",
+                "value": "[Connected Texture Packs](https://www.planetminecraft.com/texture-pack/connected-smooth-stone-no-optifine/)",
                 "inline": true
               },
               {
                 "name": ":orange_circle:",
-                "value": "New Resource Packs",
+                "value": "[New Resource Packs](https://www.planetminecraft.com/member/beatso/submissions/texture-packs/)",
                 "inline": true
               },
               {
                 "name": ":white_circle:",
-                "value": "Resource Pack Info",
+                "value": "[Resource Pack Info](https://www.beatso.tk/resource-pack-info)",
                 "inline": true
               },
               {
                 "name": ":purple_circle:",
-                "value": "Crafting Datapacks",
+                "value": "[Crafting Datapacks](https://www.planetminecraft.com/collection/69976/beatso-s-qol-crafting-packs/)",
                 "inline": true
               }
             ]
