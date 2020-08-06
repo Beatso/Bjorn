303835d8616fada3ed96369f4eaf5d507cededd3
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-06 21:12:54 +0100

    added status

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('Ready!');
-	//client.user.setActivity('@Pjotso for help', { type: 'WATCHING' });
+	client.user.setActivity('cards');
 	
 
 });
