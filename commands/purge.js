dbad02c92f6962fd3f34cf53980cf25f6e7806fa
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-06 08:43:26 +0100

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,30 @@
+const { prefix } = require('../config.json');
+
+
+module.exports = {
+    name: 'purge',
+    description: 'deletes spam and displays info',
+    usage: `${prefix}spam [messages to delete] [seconds]`,
+    cooldown: 0,
+    type: "admin",
+	execute(message, args) {
+        if (message.member.roles.cache.some(role => role.id == '739379990415540267')) {
+
+            message.channel.messages.fetch({ limit: 1 }).then(messages => {
+                
+                let messagesToDelete = args[0]
+                if (messagesToDelete == undefined) { messagesToDelete=0; } 
+                messagesToDelete++;
+                message.channel.bulkDelete(messagesToDelete);
+
+                message.channel.send(`🚫 Deleted ${messagesToDelete} messages.`).then (message => {
+                    message.delete({ timeout: 3000 });
+                });                                  
+            })
+    
+        } else {
+            message.channel.bulkDelete(1);
+            message.author.send("Stupid, don't try and use admin commands.");
+        }
+	},
+};
