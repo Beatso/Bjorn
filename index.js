950b3d03453be7078a5158dd69e72bd701754514
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-17 10:45:10 +0100

    added express server

--- index.js
+++ index.js
@@ -98,3 +98,15 @@ client.on("messageReactionRemove", async (reaction, user) => {
 })
 
 client.login(process.env.discordtoken);
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
