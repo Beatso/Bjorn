b74af67d2fbaa1c52f88b46cf2ffce74168ceebb
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-18 21:09:05 +0100

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -7,5 +7,12 @@ module.exports = {
             message.channel.send("You didn't mention a role!")
             return
         }
+        if ( args[0] = "add" ) {
+            pass
+        } else if ( args[0] = "remove" ) {
+            pass
+        } else {
+            message.channel.send("Expected `add` or `remove`. Received `"+args[0]+"`")
+        }
     },
 };
