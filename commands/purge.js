752734e8bdbb4d131dc1191b6ecb18a3b835c419
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-18 20:33:00 +0100

    fix .purge

--- commands/purge.js
+++ commands/purge.js
@@ -3,8 +3,8 @@ module.exports = {
     cooldown: 0,
     type: "admin",
 	execute(message, args) {
-		if (!message.member.roles.cache.some(role => role.id == "739379990415540267")) message.delete().then(message.channel.send("This command is only available to moderators.").then(message=>message.delete({timeout:messageTimeout}))).catch((error)=>console.error(error))
-		else if (args[0]==undefined) message.delete().then(message.channel.send("You need to specify how many messages you want to delete.").then(message=>message.delete({timeout:messageTimeout}))).catch((error)=>console.error(error))
-		else message.channel.bulkDelete(Number(args[0])+1).then(message.channel.send(`🚫 Deleted ${args[0]} messages.`).then(message=>message.delete({timeout:messageTimeout}))).catch((error)=>console.error(error))
+		if (!message.member.roles.cache.some(role => role.id == "739379990415540267")) message.delete().then(message.channel.send("This command is only available to moderators.").then(message=>message.delete({timeout:10000}))).catch((error)=>console.error(error))
+		else if (args[0]==undefined) message.delete().then(message.channel.send("You need to specify how many messages you want to delete.").then(message=>message.delete({timeout:10000}))).catch((error)=>console.error(error))
+		else message.channel.bulkDelete(Number(args[0])+1).then(message.channel.send(`🚫 Deleted ${args[0]} messages.`).then(message=>message.delete({timeout:10000}))).catch((error)=>console.error(error))
 	}
 };
