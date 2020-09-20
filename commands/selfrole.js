eca413fcaf41efa2d19ab5968fadeda7cf64cf3f
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-20 20:38:12 +0100

    fix selfrole

--- commands/selfrole.js
+++ commands/selfrole.js
@@ -3,31 +3,26 @@ module.exports = {
     cooldown: 5,
 	execute(message, args) {
 		try {
-			let roleID
-			let changeRoles
-			switch (args[0]) {
-				case "join-leave":
-					changeRoles=true
-					roleID="757303116264767519"
-				case "variated":
-					changeRoles=true
-					roleID="757302845862314035"
-				case "custom":
-					changeRoles=true
-					roleID="757303089626611754"
-				default:
-					changeRoles=false
-			}
-			if (changeRoles) {
-				const role = message.guild.roles.get(roleID)
-				if (message.member.roles.has(role)) {
-					message.member.addRole(role)
+			var roleID
+			var changeRoles
+			const roleData = [
+				{ "keyword": "join-leave", "roleID": "757303116264767519" },
+				{ "keyword": "variated", "roleID": "757302845862314035" },
+				{ "keyword": "custom", "roleID": "757303089626611754" }
+			]
+			const keywordsList = roleData.map(x=>x.keyword)
+			if (keywordsList.includes(args[0])) {
+				const roleID = roleData[keywordsList.indexOf(args[0])].roleID
+				const role = message.guild.roles.cache.get(roleID)
+				if (!message.member.roles.cache.some(x=>x.id==roleID)) {
+					message.member.roles.add(role)
 					message.channel.send(`Gave role \`${role.name}\` to <@${message.author.id}>.`)
 				} else {
-					message.member.removeRole(role)
-					message.channel.send(`Removed role \`${role.name}\` to <@${message.author.id}>.`)
+					message.member.roles.remove(role)
+					message.channel.send(`Removed role \`${role.name}\` from <@${message.author.id}>.`)
 				}
 			} else {
+				console.log(changeRoles)
 				message.reply("See this message for more info on how to use the command:\n")
 			}
 		} catch (error) {console.error(error)}
