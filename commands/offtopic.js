d0dc9dbb5e93302a0016474b9a219bd5c0d328b3
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-29 18:09:27 +0100

    fix offtopic logging

--- commands/offtopic.js
+++ commands/offtopic.js
@@ -7,7 +7,7 @@ module.exports = {
         message.delete()
             .catch((error)=>console.log(error))
             .then(()=>{
-                console.log(message.author.user.username+" triggered offtopic")
+                console.log(message.author.username+" triggered offtopic")
                 message.channel.send(`This is off topic for this channel. See the channel description for more info on how to use this channel. Off topic discussion can be continued in <#${channelIDs.offTopic}>.`)
             })
     }
