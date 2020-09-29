d5ede94f168eba5c0a5f00662f84b7129473bd66
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-29 18:06:39 +0100

    delete .offtopic command

--- commands/offtopic.js
+++ commands/offtopic.js
@@ -4,6 +4,11 @@ module.exports = {
     name: "offtopic",
     cooldown: 10,
 	execute(message, args) {
-		message.channel.send(`This is off topic for this channel. See the channel description for more info on how to use this channel. Off topic discussion can be continued in <#${channelIDs.offTopic}>.`)
+        message.delete()
+            .catch((error)=>console.log(error))
+            .then(()=>{
+                console.log(message.author.user.username+" triggered offtopic")
+                message.channel.send(`This is off topic for this channel. See the channel description for more info on how to use this channel. Off topic discussion can be continued in <#${channelIDs.offTopic}>.`)
+            })
     }
 }
