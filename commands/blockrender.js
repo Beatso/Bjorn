c316d701be45c242dde2a802fe2bb5ab3e74019e
Chromus-dev <xlchannel6@gmail.com>
2020-12-26 14:09:42 -0800

    Create blockrender.js

---
+++ commands/blockrender.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "command", 
+    execute(message, args) {
+        message.channel.send(`You can find an easy to use block renderer here: https://minerender.org`)
+    }
+}
