e81832c0fdb54b28ff73b5cc8e6cb8f5ad486036
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-12-27 11:38:13 +0000

    add skin command
    fix #21

---
+++ commands/skin.js
@@ -0,0 +1,37 @@
+const request = require("request")
+const { color } = require('../config.json')
+
+
+module.exports = {
+    name: "skin",
+	description: "Gets the skin of a Minecraft username.",
+	aliases: ["getskin", "mcskin"],
+    usage: "[username]",
+    availableTo: "@everyone",
+	execute(message, args) {
+		if (args[0] == undefined) message.channel.send("You need to specify a username!")
+		request.post("https://api.mojang.com/profiles/minecraft", { json: true, body: args[0] }, (err, res, body) => {
+			if (body.error) message.channel.send("There was an error trying to do that :c")
+			else if (body[0] == undefined) message.channel.send(`Could not find a user with the name ${args[0]}.`)
+			else {
+				const skinEmbed = {
+					"color": color,
+					"author": {
+						"name": body[0].name,
+						"icon_url": `https://crafatar.com/avatars/${body[0].id}`
+					},
+					"thumbnail": {
+						"url": `https://crafatar.com/skins/${body[0].id}`
+					},
+					"image": {
+						"url": `https://crafatar.com/renders/body/${body[0].id}`
+					},
+					"footer": {
+						"text": body[0].id
+					}
+				}
+				message.channel.send({embed:skinEmbed})
+			}
+		})
+    },
+}
