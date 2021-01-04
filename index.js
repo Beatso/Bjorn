219b6667b91698a54c128f61bc5d7e70cb2784be
Beatso <39217836+Beatso1@users.noreply.github.com>
2021-01-04 23:25:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ client.on("message", message => {
 			message.content.includes("discord.gg") ||
 			message.content.includes("discord.com/invite")
 		) &&
-		!message.member.hasPermission("MANAGE_MESSAGES")
+		!message.user.id == "634776327299399721"
 	) {
 		message.delete().then(message => message.reply("don't send invite links!"))
 	}
