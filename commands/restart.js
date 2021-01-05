0eff8696933fb4221fd8513d5f38b149e021dcf8
Beatso <39217836+Beatso1@users.noreply.github.com>
2021-01-05 09:25:11 +0000

    add restart command

---
+++ commands/restart.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'restart',
+	description: "Restarts the bot.",
+	availableTo: "<&739379990415540267>", // moderator role id
+	execute(message, args) {
+		if (!message.member.roles.cache.some(role => role.id == "739379990415540267")) message.delete().then(message.channel.send("This command is only available to moderators.").then(message=>message.delete({timeout:10000}))).catch((error)=>console.error(error)) // moderator role id
+		else {
+			process.exit()
+		}
+	}
+}
