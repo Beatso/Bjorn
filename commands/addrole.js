2c242bfed3d9ddcaff065f4e86863e1e1b431832
Beatso <39217836+Beatso1@users.noreply.github.com>
2021-01-05 12:07:39 +0000

    fix if statement

--- commands/addrole.js
+++ commands/addrole.js
@@ -7,7 +7,7 @@ module.exports = {
 	execute(message, args) {
 		if (!message.member.roles.cache.some(role => role.id == "739379990415540267")) message.delete().then(message.channel.send("This command is only available to moderators.").then(message=>message.delete({timeout:10000}))).catch((error)=>console.error(error)) // moderator role id
 		else {
-			if (args.length=0) message.channel.send("Specify a role name!")
+			if (args.length==0) message.channel.send("Specify a role name!")
 			else {
 				// add role
 				message.guild.roles.create ({
