683f8df99a675fc12ff954694d3862ac9010f261
Beatso <39217836+Beatso1@users.noreply.github.com>
2021-01-05 12:01:23 +0000

    Create addrole.js

---
+++ commands/addrole.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: 'addrole',
+	description: "Creates a new role with default permissions.",
+	aliases: ["newrole"],
+	usage: "[name]",
+	availableTo: "<&739379990415540267>", // moderator role id
+	execute(message, args) {
+		if (!message.member.roles.cache.some(role => role.id == "739379990415540267")) message.delete().then(message.channel.send("This command is only available to moderators.").then(message=>message.delete({timeout:10000}))).catch((error)=>console.error(error)) // moderator role id
+		else {
+			// add role
+			message.guild.roles.create ({
+				data : {
+					name : args.join(" ")
+				},
+				reason: `Created by ${message.author.tag}`
+			})
+				.then(role => {
+					message.channel.send(`Created role ${role.toString()}`)
+				})
+		}
+	}
+}
