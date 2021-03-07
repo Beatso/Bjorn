a54f9887c4e8be65177e490972724969704a7f65
Beatso <39217836+Beatso@users.noreply.github.com>
2021-03-07 19:11:04 +0000

    no off topic in off topic

--- commands/offtopic.js
+++ commands/offtopic.js
@@ -2,10 +2,11 @@ const {channelIDs} = require("../config.json")
 
 module.exports = {
     name: "offtopic",
-    aliases: ["ot"],
+    aliases: ["topic"],
     description: "Sends a message about a conversation being off topic.",
     availableTo: "@everyone",
 	execute(message, args) {
-        message.channel.send(`This is off topic for this channel. See the channel description for more info on how to use this channel. Off topic discussion can be continued in <#${channelIDs.offTopic}>.`)
+        if (message.channel.id == channelIDs.offTopic) message.channel.send('You can\'t be off topic in off topic.')
+        else message.channel.send(`This is off topic for this channel. See the channel description for more info on how to use this channel. Off topic discussion can be continued in <#${channelIDs.offTopic}>.`)
     }
 }
