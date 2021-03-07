47184ab0dcb5d87caefbf40810e31184b628eec2
Beatso <39217836+Beatso@users.noreply.github.com>
2021-03-07 19:07:32 +0000

    stop deleting message for ?offtopic

--- commands/offtopic.js
+++ commands/offtopic.js
@@ -6,11 +6,6 @@ module.exports = {
     description: "Sends a message about a conversation being off topic.",
     availableTo: "@everyone",
 	execute(message, args) {
-        message.delete()
-            .catch((error)=>console.log(error))
-            .then(()=>{
-                console.log(message.author.tag+" triggered offtopic")
-                message.channel.send(`This is off topic for this channel. See the channel description for more info on how to use this channel. Off topic discussion can be continued in <#${channelIDs.offTopic}>.`)
-            })
+        message.channel.send(`This is off topic for this channel. See the channel description for more info on how to use this channel. Off topic discussion can be continued in <#${channelIDs.offTopic}>.`)
     }
 }
