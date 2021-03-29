1db3710afefe5ca389cf83470fa2ba4b161a820a
Beatso <39217836+Beatso@users.noreply.github.com>
2021-03-29 02:09:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ client.once('ready', () => {
 })
 
 const tryChat = async message => {
+	if (message.guild) return
 	const req = await fetch(`https://api.monkedev.com/fun/chat?msg=${encodeURIComponent(message.content)}&uid=${message.author.id}${process.env.monkedevkey ? `&key=${process.env.monkedevkey}` : ''}`)
 		.then(res => res.json())
 		.then(json => json)
