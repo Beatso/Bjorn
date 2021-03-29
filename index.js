2e8f05fa10b08d7cf0fed96014e2907abbecd498
Beatso <39217836+Beatso@users.noreply.github.com>
2021-03-29 02:02:55 +0100

    update guild for tts

--- index.js
+++ index.js
@@ -245,7 +245,9 @@ client.on("voiceStateUpdate", (oldState, newState) => {
 
 client.on('message', async message => {
 
-	if (message.guild && !message.channel.name.startsWith('tts')) return // not in tts text channel 
+	if (!message.guild) return
+
+	if (!message.channel.name.startsWith('tts')) return // not in tts text channel 
 
 	if (message.member.voice.channelID != null && message.guild.me.voice.channelID == null) await message.member.voice.channel.join() // join the channel if not already in a channel and the user is in a channel
 	
