f1e4533051308d02302009efd5a91edb51996c8c
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-02 17:55:49 +0100

    fix info for ?updatebots

--- commands/updatebots.js
+++ commands/updatebots.js
@@ -2,9 +2,8 @@ const { exec } = require('child_process')
 
 module.exports = {
 	name: 'updatebots',
-	description: 'Runs direct JS code.',
+	description: 'Updates discord bots.',
 	availableTo: "<@634776327299399721>",
-	aliases: [],
 	async execute(message, args) {
 
 		if (message.author.id !== '634776327299399721') return message.channel.send('You don\'t have permission to do that!')
