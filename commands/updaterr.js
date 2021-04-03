47fc8966f18c610786267805df126a27900c8ae5
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-03 23:17:59 +0100

    mhm

--- commands/updaterr.js
+++ commands/updaterr.js
@@ -8,16 +8,14 @@ module.exports = {
 
                 if (message.author.id != "634776327299399721") return
 
-
-                console.log("idhcisdfh")
                 let notificationDesc = 'React to this message to get roles to be pinged for announcements and events.'
-                reactionRoleData.filter(e => e.type == "notification").forEach(e => notificationDesc += `\n${e.emoji} <@${e.role}>`)
+                reactionRoleData.filter(e => e.type == "notification").forEach(e => notificationDesc += `\n${e.emoji} <@&${e.role}>`)
 
                 let accessDesc = 'React to this message to get roles to see certain channels.'
-                reactionRoleData.filter(e => e.type == "access").forEach(e => accessDesc += `\n${e.emoji} <@${e.role}>`)
+                reactionRoleData.filter(e => e.type == "access").forEach(e => accessDesc += `\n${e.emoji} <@&${e.role}>`)
 
                 let pronounDesc = 'React to this message to get roles to show people your pronouns.'
-                reactionRoleData.filter(e => e.type == "pronoun").forEach(e => pronounDesc += `\n${e.emoji} <@${e.role}>`)
+                reactionRoleData.filter(e => e.type == "pronoun").forEach(e => pronounDesc += `\n${e.emoji} <@&${e.role}>`)
 
                 const notificationEmbed = {
                         "title": "🔔 Notification Roles",
@@ -38,7 +36,6 @@ module.exports = {
                 const getRolesChannel = (await message.guild.channels.cache.get("740838518116319322"))
 
                 const notificationMessage = (await getRolesChannel.send({embed: notificationEmbed}))
-                console.log(notificationMessage)
                 reactionRoleData.filter(e => e.type == "notification").forEach(async e => {await notificationMessage.react(e.emoji)})
 
                 const accessMessage = (await getRolesChannel.send({embed: accessEmbed}))
@@ -46,7 +43,6 @@ module.exports = {
 
                 const pronounsMessage = (await getRolesChannel.send({embed: pronounsEmbed}))
                 reactionRoleData.filter(e => e.type == "pronouns").forEach(async e => {await pronounsMessage.react(e.emoji)})
-                console.log("asosad")
 
         },
 };
