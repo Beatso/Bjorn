5b583cb9f503baace4a0bf410579593f0f9a5883
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-04 12:03:22 +0100

    use embeds for base64 and new encode/decode functions

--- commands/base64.js
+++ commands/base64.js
@@ -1,44 +1,30 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
 	name: 'base64',
 	description: 'Encodes and Decodes base64',
 	usage: `<encode | decode> <text>`,
 	execute(message, args) {
+
+		args[0] = args[0].toLowerCase()
+
 		if (!args[0]) return message.reply('You must specify encoding or decoding.');
-		if (!(args[0] == 'encode' || args[0] == 'decode'))
-			return message.reply('You must specify encoding or decoding.');
+		if (!(args[0] == 'encode' || args[0] == 'decode')) return message.reply('You must specify encoding or decoding.');
 		if (!args[1]) return message.reply(`You must specify text to ${args[0]}.`);
 
-		var text = args.join(' ').substring(args[0].length + 1);
+		const embed = new MessageEmbed().setTitle(`Base 64 ${args[0]}`).setColor(message.guild ? message.guild.me.displayHexColor : '')
 
-		function encode(text) {
-			let buff = new Buffer.from(text);
-			let base64data = buff.toString('base64');
-			let res = {
-				originalData: text,
-				buff: buff,
-				base64: base64data
-			};
-			return res;
-		}
-		function decode(text) {
-			let buff = new Buffer.from(text, 'base64');
-			let base64data = buff.toString('ascii');
-			let res = {
-				originalData: text,
-				buff: buff,
-				base64: base64data
-			};
-			return res;
-		}
+		const text = args.join(' ').substring(args[0].length + 1);
 
 		try {
-			if (args[0] == 'encode') {
-				let res = encode(text);
-				message.channel.send(res.base64, { disableMentions: 'all' });
-			} else {
-				let res = decode(text);
-				message.channel.send(res.base64, { disableMentions: 'all' });
-			}
+
+			if (args[0] == 'encode')
+				embed.setDescription((new Buffer.from(text)).toString('base64'))
+			else
+				embed.setDescription((new Buffer.from(text, 'base64')).toString('ascii'))
+		
+		message.channel.send(embed)
+			
 		} catch (err) {
 			message.reply(`There was an error ${args[0].substring(0, args[0].length - 1)}ing that.`);
 			console.log(err);
