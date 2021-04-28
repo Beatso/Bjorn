264cd360fe4ee2121ae9abafb218fd60bc6902b4
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-28 16:52:15 +0100

    fix args parsing for ?assets fixes #76

--- commands/assets.js
+++ commands/assets.js
@@ -5,7 +5,7 @@ module.exports = {
 	usage: "[version]",
 	availableTo: "@everyone",
     execute(message, args) {
-		if (!args[0]) "You need to provide a version!"
+		if (!args[0]) return message.channel.send("You need to provide a version!")
 		message.channel.send(`Browse the assets for version ${args[0]}: <https://github.com/InventivetalentDev/minecraft-assets/tree/${encodeURI(args[0])}>\nDirect download link: https://github.com/InventivetalentDev/minecraft-assets/archive/${encodeURI(args[0])}.zip\nNote: these links will not work correctly if an invalid version was specified.`)
 	},
 };
